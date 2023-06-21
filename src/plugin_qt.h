3f728fa6918aef2c249bdf18fccda1c898faadf8
MichaelM <mmeikle3@gmail.com>
2023-06-21 18:51:42 -0400

    Simple API Version Update to ensure compatibility

--- src/plugin_qt.h
+++ src/plugin_qt.h
@@ -15,7 +15,7 @@ public:
 
 	static const char* kPluginName;
 	static const char* kPluginVersion;
-	static const int kPluginApiVersion = 23;
+	static const int kPluginApiVersion = 26;
 	static const char* kPluginAuthor;
 	static const char* kPluginDescription;
 
